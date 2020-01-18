08ba43d2f5d8d9c2a73e5f77ed212845ad0eb406
Marko Djordjevic <markobombas@gmail.com>
2020-01-18 21:46:43 +0100

    Zapoceta izmena

--- src/main/java/zadatak/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadatak/Zadatak3IzmenaVrednosti.java
@@ -1,13 +1,25 @@
 package zadatak;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Roba;
+
+import java.util.List;
 
 public class Zadatak3IzmenaVrednosti {
+    public static Dao<Roba, Integer>daoroba;
     public static void main(String[] args) {
         ConnectionSource cs = null;
         try {
             cs = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+            DaoManager.createDao(cs, Roba.class);
+            List<Roba>roba = daoroba.queryForEq(Roba.POLJE_OPIS, "Plasticna stolica");
+            roba.get(0);
+
+
+
 
         }catch (Exception e){
             e.printStackTrace();
